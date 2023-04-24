01c8edb29d133a8996620a51237ae5f6fc7883b7
JaFi01 <116461510+JaFi01@users.noreply.github.com>
2023-04-24 21:43:32 +0200

    Added filter to General Feed module

--- src/components/GeneralFeed.js
+++ src/components/GeneralFeed.js
@@ -2,17 +2,58 @@ import React, { useState, useEffect } from "react";
 
 function GeneralFeed() {
   const [posts, setPosts] = useState([]);
+  const [minChars, setMinChars] = useState(0);
+  const [maxChars, setMaxChars] = useState(1000);
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((data) => setPosts(data));
+      .then((data) => {
+        setPosts(data);
+        setFilteredPosts(data);
+      });
   }, []);
+
+  const handleMinCharsChange = (event) => {
+    setMinChars(event.target.value);
+  };
+
+  const handleMaxCharsChange = (event) => {
+    setMaxChars(event.target.value);
+  };
+
+  const handleShowPosts = () => {
+    const filtered = posts.filter((post) => {
+      const bodyLength = post.body.length;
+      return bodyLength > minChars && bodyLength < maxChars;
+    });
+    setFilteredPosts(filtered);
+  };
 
   return (
     <div>
       <h2>General Feed</h2>
-      {posts.map((post) => (
+      <label>
+        Minimalna ilość znaków:
+        <input
+          type="number"
+          value={minChars}
+          onChange={handleMinCharsChange}
+        />
+      </label>
+      <br />
+      <label>
+        Maksymalna ilość znaków:
+        <input
+          type="number"
+          value={maxChars}
+          onChange={handleMaxCharsChange}
+        />
+      </label>
+      <br />
+      <button onClick={handleShowPosts}>Pokaż posty</button>
+      {filteredPosts.map((post) => (
         <div key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
